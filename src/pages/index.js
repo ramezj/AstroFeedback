f28f1759a171cb8b2ce7cfebc009fa131b2b86a5
Lunix <ramezjoseph8@gmail.com>
2023-06-27 01:46:22 +0200

    [src] prisma adp

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import React, { useState, useEffect } from 'react';
+import Button from "../components/login-btn";
 
 export default function Home() {
   return (
     <>
     <h1>Hello from Next js deployed to Railway.app</h1>
+    <Button />
     </>
   )
 }
