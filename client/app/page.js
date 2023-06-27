eebd8f60c59af5a736c4bac65eed32c3d4faa9fe
Lunix <ramezjoseph8@gmail.com>
2023-06-27 18:29:04 +0200

    [AstroFeedback] Navbar, Bg

--- client/app/page.js
+++ client/app/page.js
@@ -1,11 +1,12 @@
 "use client"
 import { useSession, signIn, signOut } from "next-auth/react"
+import Image from 'next/image'
 import Navbar from "@/components/Navbar"
 
 export default function Component() {
   return (
-    <>
+    <div className='bg-[url("/blue.png")] h-full min-h-screen bg-fill bg-no-repeat w-full'>
     <Navbar />
-    </>
+    </div>
   )
 }
--- client/components/Navbar/index.js
+++ client/components/Navbar/index.js
@@ -4,12 +4,12 @@ export default function Navbar() {
   const { data: session } = useSession()
     return (
         <>
-        <div className="navbar bg-base-100 mt-2">
+        <div className="navbar bg-transparent">
   <div className="flex-1">
-    <a className="btn btn-ghost normal-case text-xl ml-1">daisyUI</a>
+    <a className="btn btn-ghost normal-case text-xl ml-1 mt-2">🚀 AstroFeedback</a>
   </div>
   <div className="flex-none gap-2">
-    <div className="dropdown dropdown-end mr-1">
+    <div className="dropdown dropdown-end mr-1 mt-2">
       {
         session 
         ? <><button class="btn btn-primary normal-case">Dashboard</button></>
