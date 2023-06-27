2cfe90d9fe0e70cc8bffa05c13d565065a4a92a3
Lunix <ramezjoseph8@gmail.com>
2023-06-27 13:29:45 +0200

    [Client] Api Update

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,16 +1,15 @@
-"use client"
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { SessionProvider } from "next-auth/react"
+import Provider from '@/components/Provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children,  session }) {
   return (
-    <SessionProvider session={session}>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Provider>{children}</Provider>
+        </body>
     </html>
-    </SessionProvider>
   )
 }
