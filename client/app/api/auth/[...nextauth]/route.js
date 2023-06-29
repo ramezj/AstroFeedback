ea623fbaa77e483583193133657930815061498d
Lunix <ramezjoseph8@gmail.com>
2023-06-29 13:45:46 +0200

    [Next-Auth] Email Provider

--- client/app/api/auth/[...nextauth]/route.js
+++ client/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import GithubProvider from "next-auth/providers/github"
+import EmailProvider from "next-auth/providers/email";
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 
@@ -10,16 +12,25 @@ export const authOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        })
+        }),
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
+        }),
+        EmailProvider({
+            server: process.env.EMAIL_SERVER,
+            from: process.env.EMAIL_FROM
+          }),
     ],
-    callbacks: {
-        async session ({session, token, user}) {
-            session.accessToken = token
-            session.user.id = user.id
-            return session
-        }
+    session: {
+        strategy: "jwt",
     },
+    // pages: {
+    //     signIn: "/login",
+    // },
     adapter: PrismaAdapter(prisma),
+    secret: process.env.NEXTAUTH_SECRET,
+
 }
 const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
